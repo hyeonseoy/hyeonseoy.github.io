799e14c672ad5ce40b05259843accf64f5bbe812
hyeonseoy <hyeonseoy000@gmail.com>
2026-05-12 15:31:22 +0900

    delete explorer

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -54,7 +54,6 @@ export const defaultContentPageLayout: PageLayout = {
         { Component: Component.ReaderMode() },
       ],
     }),
-    Component.Explorer(),
   ],
   right: [
     Component.Graph(),
