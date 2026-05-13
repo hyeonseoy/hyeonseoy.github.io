acd9958f7c9d3f5d5e73a8285fdf03a46cf02d91
hyeonseoy <hyeonseoy000@gmail.com>
2026-05-14 00:01:23 +0900

    update

--- quartz.layout.ts
+++ quartz.layout.ts
@@ -4,23 +4,26 @@ import * as Component from "./quartz/components"
 // components shared across all pages
 export const sharedPageComponents: SharedLayout = {
   head: Component.Head(),
+
   header: [],
+
   afterBody: [
-	Component.Comments({
-		provider: "giscus",
-		options: {
-			repo: "hyeonseoy/hyeonseoy.github.io",
-			repoId: "R_kgDOSZeQsA",
-			category: "General",
-			categoryId: "DIC_kwDOSZeQsM4C8yaV",
-			mapping: "pathname",
-			strict: false,
-			reactionsEnabled: true,
-			inputPosition: "bottom",
-			lang: "ko",
-		},
-	}),
+    Component.Comments({
+      provider: "giscus",
+      options: {
+        repo: "hyeonseoy/hyeonseoy.github.io",
+        repoId: "R_kgDOSZeQsA",
+        category: "General",
+        categoryId: "DIC_kwDOSZeQsM4C8yaV",
+        mapping: "pathname",
+        strict: false,
+        reactionsEnabled: true,
+        inputPosition: "bottom",
+        lang: "ko",
+      },
+    }),
   ],
+
   footer: Component.Footer({
     links: {
       GitHub: "https://github.com/jackyzha0/quartz",
@@ -33,57 +36,83 @@ export const sharedPageComponents: SharedLayout = {
 export const defaultContentPageLayout: PageLayout = {
   beforeBody: [
     Component.ArticleTitle(),
+
     Component.ContentMeta(),
+
     Component.TagList(),
 
-	Component.ConditionalRender({
-		component: Component.RecentNotes({
-			limit: 10,
-			filter: (page) =>
-				page.fileData.slug !== "index"
-		}),
-		condition: (page) => page.fileData.slug === "index",
-	}),
+    Component.ConditionalRender({
+      component: Component.RecentNotes({
+        limit: 10,
+
+        filter: (page) =>
+          page.fileData.slug !== "index",
+      }),
+
+      condition: (page) => page.fileData.slug === "index",
+    }),
   ],
+
   left: [
     Component.PageTitle(),
-	Component.Socials(),
+
+    Component.Socials(),
+
     Component.MobileOnly(Component.Spacer()),
+
     Component.Flex({
       components: [
         {
           Component: Component.Search(),
           grow: true,
         },
+
         { Component: Component.Darkmode() },
+
         { Component: Component.ReaderMode() },
       ],
     }),
-	Component.Explorer(),
+
+    Component.Explorer(),
   ],
+
   right: [
     Component.Graph(),
+
     Component.DesktopOnly(Component.TableOfContents()),
-	Component.Backlinks(),
+
+    Component.Backlinks(),
   ],
 }
 
-// components for pages that display lists of pages  (e.g. tags or folders)
+// components for pages that display lists of pages (e.g. tags or folders)
 export const defaultListPageLayout: PageLayout = {
-  beforeBody: [Component.Breadcrumbs(), Component.ArticleTitle(), Component.ContentMeta(),],
+  beforeBody: [
+    Component.Breadcrumbs(),
+
+    Component.ArticleTitle(),
+
+    Component.ContentMeta(),
+  ],
+
   left: [
     Component.PageTitle(),
+
     Component.MobileOnly(Component.Spacer()),
+
     Component.Flex({
       components: [
         {
           Component: Component.Search(),
           grow: true,
         },
+
         { Component: Component.Darkmode() },
       ],
     }),
+
     Component.Explorer(),
   ],
+
   right: [],
 }
